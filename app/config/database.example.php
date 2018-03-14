63e43acd308854ef61500cae257acd415cb39b1d
Kitutu <pkitutu@gmail.com>
2018-03-14 14:30:51 +0300

    git ignore database.php

--- app/config/database.example.php
+++ app/config/database.example.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'iblis',
 			'username'  => 'root',
-			'password'  => 'mysql',
+			'password'  => 'xx',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
